ce6901e582539417eec0102ed675465525e1e4e0
Radha <Radha4736@gmail.com>
2017-07-19 23:59:21 +0530

    login form using input form and angular

---
+++ main.js
@@ -0,0 +1,8 @@
+//$(document).ready(function () {
+//  var email = $('#user-email').val();
+//  var message = 'welcome back,' + email;
+  //$('.welcome-message').text(message);
+//  console.log($('#user-email').val());
+
+//});   11
+//});
