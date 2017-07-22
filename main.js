715343632d94fe2b17b7eeea4f6b5031412c75f8
Radha <Radha4736@gmail.com>
2017-07-22 20:32:31 +0530

    displayed the information of restaurant using array of object nd ng-bind

--- main.js
+++ main.js
@@ -9,6 +9,15 @@
 var FoodApp = angular.module('foodieapp', []);
 FoodApp.controller('mainController', function($scope){
 
-$scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
-
+  $scope.restaurants = [{
+  	name: 'Farzi Cafe',
+  	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+  	location: 'Connaught Place',
+  	category: 'Casual Dining, Bar',
+  	vote: '4.2',
+  	cuisines: 'Modern Indian',
+  	cost: '2200',
+  	hours: '12 Noon to 1 AM (Mon-Sun)',
+  	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+  }]
 })
